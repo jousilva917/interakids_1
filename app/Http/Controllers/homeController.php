65694d63514e62ace315bb57cd0d7a298de57748
TheWinterhell <raphaelcoelho46@gmail.com>
2018-04-23 21:04:49 -0300

    Delete controller

--- app/Http/Controllers/homeController.php
+++ app/Http/Controllers/homeController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-class homeController extends Controller
+class HomeController extends Controller
 {
     /**
      * Display a listing of the resource.
@@ -13,7 +13,7 @@ class homeController extends Controller
      */
     public function index()
     {
-        return('website.contact');
+        return('website.home');
     }
 
     /**
