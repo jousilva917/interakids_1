f4ef730fbae4bbc46ba8cbfca607ffb38666cf31
TheWinterhell <raphaelcoelho46@gmail.com>
2018-04-23 21:06:23 -0300

    bla

--- app/Http/Controllers/homeController.php
+++ app/Http/Controllers/homeController.php
@@ -13,7 +13,7 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return('website.home');
+        return('website.index');
     }
 
     /**
--- routes/web.php
+++ routes/web.php
@@ -10,12 +10,4 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-Route::get('/' , function () {
-    return '(/layouts/website)';
-});
-Route::resource('home', 'homeController');
-Route::get('/about', 'homeController@getAbout')->name('about');
-Route::get('/contact', 'homeController@getContact')->name('contact');
-Route::get('/explore', 'exploreController@getExplore')->name('explore');
-Route::get('/learn', 'learnControllerr@getLearn')->name('learn');
-
+Route::resource('Home', 'HomeController');
