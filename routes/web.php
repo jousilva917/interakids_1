f7cb70c5b4c42b29c83f2a6bda561d4b9a534b87
TheWinterhell <raphaelcoelho46@gmail.com>
2018-04-25 03:51:01 -0300

    InteraKids

--- routes/web.php
+++ routes/web.php
@@ -1,25 +1,21 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
 
 /*Route::namespace('Home')->group(function () {
     // Controllers Within The "App\Http\Controllers\Home" Namespace*/
-    Route::group(['namespace' => 'Home'], function()
-    {
-    Route::get('/', function()
-    {
-        return redirect()->action('Home\HomeController@index');
-    });
-    Route::resource('home', 'HomeController');
+
+Route::get('/', function()
+{
+    return redirect()->action('HomeController@index');
 });
+Route::get('/home', 'HomeController@index');
+
 Route::resource('explore', 'ExploreController');
+
 Route::resource('learn', 'LearnController');
+
+
+//Sistema de cadastro
+Auth::routes();
+
+Route::get('/perfil', 'HomeController@perfil')->name('perfil');
