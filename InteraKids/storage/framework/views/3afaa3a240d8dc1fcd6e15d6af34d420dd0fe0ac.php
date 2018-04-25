0891955773f08e7bd6ac806f8037f810e463dd83
TheWinterhell <raphaelcoelho46@gmail.com>
2018-04-25 03:21:18 -0300

    Delete 3afaa3a240d8dc1fcd6e15d6af34d420dd0fe0ac.php

--- InteraKids/storage/framework/views/3afaa3a240d8dc1fcd6e15d6af34d420dd0fe0ac.php
+++
@@ -1,6 +0,0 @@
-<!-- Bootstrap CSS -->
-<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css" integrity="sha384-PsH8R72JQ3SOdhVi3uxftmaW6Vc51MKb0q5P2rRUpPvrszuE4W1povHYgTpBfshb" crossorigin="anonymous">
-
-<link rel="stylesheet" href="<?php echo e(asset('css/temp.css')); ?>">
-
-<?php echo $__env->yieldContent('styles'); ?>
