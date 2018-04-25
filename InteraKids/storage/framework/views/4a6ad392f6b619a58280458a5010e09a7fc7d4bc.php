10dbed8509591c1c22c6541462596b194cc7ca01
TheWinterhell <raphaelcoelho46@gmail.com>
2018-04-25 03:21:26 -0300

    Delete 4a6ad392f6b619a58280458a5010e09a7fc7d4bc.php

--- InteraKids/storage/framework/views/4a6ad392f6b619a58280458a5010e09a7fc7d4bc.php
+++
@@ -1,4 +0,0 @@
-<?php $__env->startSection('content'); ?>
-<?php $__env->stopSection(); ?>
-
-<?php echo $__env->make('layouts.website', array_except(get_defined_vars(), array('__data', '__path')))->render(); ?>
