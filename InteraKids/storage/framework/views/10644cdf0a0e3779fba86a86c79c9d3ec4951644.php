9737389b21d9d79e9f1e737bad6d17b75c68b65b
TheWinterhell <raphaelcoelho46@gmail.com>
2018-04-25 03:20:48 -0300

    Delete 10644cdf0a0e3779fba86a86c79c9d3ec4951644.php

--- InteraKids/storage/framework/views/10644cdf0a0e3779fba86a86c79c9d3ec4951644.php
+++
@@ -1,7 +0,0 @@
-<title>InteraKids - <?php echo $__env->yieldContent('subtitle'); ?></title>
-
-<!-- Required meta tags -->
-<meta charset="utf-8">
-<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-<?php echo $__env->make('partials.styles', array_except(get_defined_vars(), array('__data', '__path')))->render(); ?>
