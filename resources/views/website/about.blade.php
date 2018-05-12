4c1fde7b5f614e1f779aa8c19569f4a1e89de78f
TheWinterhell <raphaelcoelho46@gmail.com>
2018-05-12 12:25:53 -0300

    InteraKids

--- resources/views/website/about.blade.php
+++ resources/views/website/about.blade.php
@@ -7,61 +7,110 @@
                <h1>Sobre</h1>
             </div>
         </div>
-        <div class="container">
-                <div class="card-group">
-                        <div class="card">
-                          <img class="card-img-top" src="http://www.hackster.org/wp-content/uploads/2017/07/Javascript-768x513.jpg" alt="Card image cap">
-                          <div class="card-body">
-                            <h5 class="card-title">Bruno Grabriel</h5>
-                            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                            <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-                          </div>
-                        </div>
-                        <div class="card">
-                          <img class="card-img-top" src="http://www.hackster.org/wp-content/uploads/2017/07/Javascript-768x513.jpg" alt="Card image cap">
-                          <div class="card-body">
-                            <h5 class="card-title">João Vitor Coutinho</h5>
-                            <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
-                            <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-                          </div>
-                        </div>
-                        <div class="card">
-                          <img class="card-img-top" src="http://www.hackster.org/wp-content/uploads/2017/07/Javascript-768x513.jpg" alt="Card image cap">
-                          <div class="card-body">
-                            <h5 class="card-title">Jonathan Micael</h5>
-                            <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
-                            <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-                          </div>
-                        </div>
+        <div class="album py-5 bg-light">
+          <div class="container">
+  
+            <div class="row">
+              <div class="col-md-4">
+                <div class="card mb-4 box-shadow">
+                  <img class="card-img-top" src="http://www.hackster.org/wp-content/uploads/2017/07/Javascript-768x513.jpg" alt="Card image cap">
+                  <div class="card-body">
+                      <h5 class="card-title">Bruno Gabriel</h5>
+                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                    <div class="d-flex justify-content-between align-items-center">
+                      <div class="btn-group">
+                        <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
+                        <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
                       </div>
-                      <div class="card-group">
-                      <div class="card">
-                            <img class="card-img-top" src="http://www.hackster.org/wp-content/uploads/2017/07/Javascript-768x513.jpg" alt="Card image cap">
-                            <div class="card-body">
-                              <h5 class="card-title">Mikaela Teodoro</h5>
-                              <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-                              <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-                            </div>
-                          </div>
-                          <div class="card">
-                            <img class="card-img-top" src="http://www.hackster.org/wp-content/uploads/2017/07/Javascript-768x513.jpg" alt="Card image cap">
-                            <div class="card-body">
-                              <h5 class="card-title">Silvio Raphael</h5>
-                              <p class="card-text">This card has supporting text below as a natural lead-in to additional content.</p>
-                              <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-                            </div>
-                          </div>
-                          <div class="card">
-                            <img class="card-img-top" src="http://www.hackster.org/wp-content/uploads/2017/07/Javascript-768x513.jpg" alt="Card image cap">
-                            <div class="card-body">
-                              <h5 class="card-title">Vinicius Tavares</h5>
-                              <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This card has even longer content than the first to show that equal height action.</p>
-                              <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
-                            </div>
-                          </div>
-                        </div>
+                      <small class="text-muted">9 mins</small>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="col-md-4">
+                <div class="card mb-4 box-shadow">
+                  <img class="card-img-top" src="http://www.hackster.org/wp-content/uploads/2017/07/Javascript-768x513.jpg" alt="Card image cap">
+                  <div class="card-body">
+                      <h5 class="card-title">João Vitor Coutinho</h5>
+                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                    <div class="d-flex justify-content-between align-items-center">
+                      <div class="btn-group">
+                        <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
+                        <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
+                      </div>
+                      <small class="text-muted">9 mins</small>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="col-md-4">
+                <div class="card mb-4 box-shadow">
+                  <img class="card-img-top" src="http://www.hackster.org/wp-content/uploads/2017/07/Javascript-768x513.jpg" alt="Card image cap">
+                  <div class="card-body">
+                      <h5 class="card-title">Jonathan Micael</h5>
+                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                    <div class="d-flex justify-content-between align-items-center">
+                      <div class="btn-group">
+                        <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
+                        <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
+                      </div>
+                      <small class="text-muted">9 mins</small>
+                    </div>
+                  </div>
+                </div>
+              </div>
+  
+              <div class="col-md-4">
+                <div class="card mb-4 box-shadow">
+                  <img class="card-img-top" src="http://www.hackster.org/wp-content/uploads/2017/07/Javascript-768x513.jpg" alt="Card image cap">
+                  <div class="card-body">
+                      <h5 class="card-title">Mikaela Teodoro</h5>
+                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                    <div class="d-flex justify-content-between align-items-center">
+                      <div class="btn-group">
+                        <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
+                        <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
+                      </div>
+                      <small class="text-muted">9 mins</small>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="col-md-4">
+                <div class="card mb-4 box-shadow">
+                  <img class="card-img-top" src="http://www.hackster.org/wp-content/uploads/2017/07/Javascript-768x513.jpg" alt="Card image cap">
+                  <div class="card-body">
+                      <h5 class="card-title">Silvio Raphael</h5>
+                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                    <div class="d-flex justify-content-between align-items-center">
+                      <div class="btn-group">
+                        <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
+                        <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
+                      </div>
+                      <small class="text-muted">9 mins</small>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <div class="col-md-4">
+                <div class="card mb-4 box-shadow">
+                  <img class="card-img-top" src="http://www.hackster.org/wp-content/uploads/2017/07/Javascript-768x513.jpg" alt="Card image cap">
+                  <div class="card-body">
+                    <h5 class="card-title">Vinícius Tavares</h5>
+                    <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                    <div class="d-flex justify-content-between align-items-center">
+                      <div class="btn-group">
+                        <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
+                        <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
+                      </div>
+                      <small class="text-muted">9 mins</small>
+                    </div>
+                  </div>
+                </div>
+              </div>              
+          </div>
         </div>
-            </div>
+        </div>
         @include('partials.footer')
     </body>
 </html>
