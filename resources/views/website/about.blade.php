c9de5ea12ef35006ca2ffe7fbe5c5e171b87180c
TheWinterhell <raphaelcoelho46@gmail.com>
2018-05-12 01:38:57 -0300

    InterKids

--- resources/views/website/about.blade.php
+++ resources/views/website/about.blade.php
@@ -2,11 +2,21 @@
     @include('partials.head')
     <body>
         @include('partials.nav')
-        <header class="about">
+        <div class="about">
             <div class="landing">
                <h1>Sobre</h1>
             </div>
-        </header>
+        </div>
+        <div class="container">
+        <div class="card" style="width: 18rem;">
+                <img class="card-img-top" src="..." alt="Card image cap">
+                <div class="card-body">
+                  <h5 class="card-title">Card title</h5>
+                  <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                  <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+              </div>
+            </div>
         @include('partials.footer')
     </body>
 </html>
