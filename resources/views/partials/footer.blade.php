a3bab876975f54f341718039512b06dd9e59a162
TheWinterhell <raphaelcoelho46@gmail.com>
2018-05-11 22:09:46 -0300

    InteraKids

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <footer>
     <div class="footer-above">
         <div class="container">
@@ -19,25 +18,3 @@
         </div>
     </div>
 </footer>
-=======
-<footer>
-  <div class="footer-above">
-    <div class="container">
-      <div class="row">
-        <div class="col-md-4 footer-col">footer-col 1</div>
-        <div class="col-md-4 footer-col">footer-col 2</div>
-        <div class="col-md-4 footer-col">footer-col 3</div>
-      </div>
-    </div>
-  </div>
-  <div class="footer-below">
-    <div class="container">
-      <div class="row">
-        <div class="col-md-12 text-center">
-          Footer
-        </div>
-      </div>
-    </div>
-  </div>
-</footer>
->>>>>>> d41c6177bf0e06020866d54c15ec8231dc2dbe3a
--- resources/views/partials/nav.blade.php
+++ resources/views/partials/nav.blade.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
     <!-- Navegação -->
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
         <div class="container-fluid">
@@ -61,67 +60,3 @@
             </div>
         </div>
     </nav>
-=======
-<!-- Navegação -->
-<nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
-  <div class="container-fluid">
-      <a class="navbar-brand" href="{{ url('/') }}">
-          {{ config('app.name', 'InteraKids') }}
-      </a>
-      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-      </button>
-
-      <div class="collapse navbar-collapse" id="navbarSupportedContent">
-          <!-- Left Side Of Navbar -->
-          <ul class="navbar-nav mr-auto">
-
-          </ul>
-
-          <!-- Right Side Of Navbar -->
-          <ul class="navbar-nav ml-auto">
-              <li class="nav-item active">
-                  <a class="nav-link" href="{{ url('home') }}">Home
-                    <span class="sr-only">(current)</span>
-                  </a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="{{ url('learn') }}">Aprenda</a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="{{ url('explore') }}">Explore</a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="{{ url('about') }}">Sobre</a>
-                </li>
-                <li class="nav-item">
-                  <a class="nav-link" href="{{ url('contact') }}">Contato</a>
-                </li>
-              <!-- Authentication Links -->
-              @guest
-                  <li><a class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a></li>
-                  <li><a class="nav-link" href="{{ route('register') }}">{{ __('Registrar') }}</a></li>
-              @else
-                  <li class="nav-item dropdown">
-                      <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                          {{ Auth::user()->name }} <span class="caret"></span>
-                      </a>
-
-                      <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                          <a class="dropdown-item" href="{{ route('logout') }}"
-                             onclick="event.preventDefault();
-                                           document.getElementById('logout-form').submit();">
-                              {{ __('Logout') }}
-                          </a>
-
-                          <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                              @csrf
-                          </form>
-                      </div>
-                  </li>
-              @endguest
-          </ul>
-      </div>
-  </div>
-</nav>
->>>>>>> d41c6177bf0e06020866d54c15ec8231dc2dbe3a
