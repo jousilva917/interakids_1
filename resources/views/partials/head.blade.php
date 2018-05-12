713fcd00e56a3457a66f3b067b1c3ee05704f0c0
TheWinterhell <raphaelcoelho46@gmail.com>
2018-05-11 22:15:44 -0300

    InteraKids

--- resources/views/partials/head.blade.php
+++ resources/views/partials/head.blade.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 <!-- Required meta tags -->
 <title>{{ config('app.name', 'InteraKids') }}</title>
 <meta charset="utf-8">
@@ -15,11 +14,3 @@
 
 <script src="{{ asset('js/app.js') }}" defer></script>
 
-=======
-
-<!-- Required meta tags -->
-<meta charset="utf-8">
-<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-
-@include('partials.styles')
->>>>>>> d41c6177bf0e06020866d54c15ec8231dc2dbe3a
