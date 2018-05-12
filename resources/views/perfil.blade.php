bf75577e43c73b7f344ab33a05d0f733ce807f5e
TheWinterhell <raphaelcoelho46@gmail.com>
2018-05-11 22:16:52 -0300

    InteraKids

--- resources/views/perfil.blade.php
+++ resources/views/perfil.blade.php
@@ -9,15 +9,9 @@
 
                 <div class="card-body">
                     @if (session('status'))
-<<<<<<< HEAD
                     <div class="alert alert-success">
                         {{ session('status') }}
                     </div>
-=======
-                        <div class="alert alert-success">
-                            {{ session('status') }}
-                        </div>
->>>>>>> d41c6177bf0e06020866d54c15ec8231dc2dbe3a
                     @endif
 
                     You are logged in!
