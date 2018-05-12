6ae7185752fd8e1e9e129612675c2516694139b8
TheWinterhell <raphaelcoelho46@gmail.com>
2018-05-11 21:59:04 -0300

    InteraKids

--- resources/views/layouts/website.blade.php
+++ resources/views/layouts/website.blade.php
@@ -1,5 +1,3 @@
-<<<<<<< HEAD
-<!DOCTYPE html>
 <html>
     @include('partials.head')
     <body>
@@ -48,61 +46,3 @@
         @include('partials.footer')
     </body>
 </html>
-=======
-<!DOCTYPE html>
-<html>
-    @include('partials.head')
-		<!-- Required meta tags -->
-		<meta charset="utf-8">
-		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-	<body>
-    <!-- Navegação -->
-    @include('partials.nav')
-
-		@yield('content')
-    <header class="masthead">
-    		    <div class="container">
-    		      <div class="row">
-    		        <div class="col-md-12">
-									<h1>Slider principal</h1>
-    		        </div>
-    		      </div>
-    		    </div>
-    		  </header>
-
-    		  <section id="video">
-    		    <div class="container">
-    		      <h1>Video</h1>
-    		      <div class="row">
-    		        <div class="col-md-12">
-
-    		        </div>
-    		      </div>
-    		    </div>
-    		  </section>
-
-    		  <section id="grid-app">
-    		    <div class="container">
-    		      <h1>Grid</h1>
-    		      <div class="row">
-    		        <div class="col-md-12">
-
-    		        </div>
-    		      </div>
-    		    </div>
-    		  </section>
-
-    		  <section id="sobre">
-    		    <div class="container">
-    		      <h1>sobre</h1>
-    		      <div class="row">
-    		        <div class="col-md-12">
-
-    		        </div>
-    		      </div>
-    		    </div>
-    		  </section>
-			@include('partials.footer')
-	</body>
-</html>
->>>>>>> d41c6177bf0e06020866d54c15ec8231dc2dbe3a
