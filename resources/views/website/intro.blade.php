7a8755ebfe0bc4530ce74b70e54f7d262cc2637e
Jonathan <jonstadeepin@gmail.com>
2018-05-17 20:49:22 -0300

    interaKids

--- resources/views/website/intro.blade.php
+++ resources/views/website/intro.blade.php
@@ -1,6 +1,31 @@
-<link rel="stylesheet" href="{{ asset('css/intro.css') }}">
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js">
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>{{config('app.name', 'InteraKids')}}</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="{{ asset('css/intro.css') }}">
+    <link rel="stylesheet" href="{{ asset('js/intro.js') }}">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<script type="text/javascript">
 
+    $(function(){
+    var welcomeSection = $('.welcome-section'),
+    enterButton = welcomeSection.find('.enter-button');
+    
+    setTimeout(function(){
+        welcomeSection.removeClass('content-hidden');
+    
+    },800);
+    
+    enterButton.on('click', function(e){
+    e.preventDefault();
+    welcomeSection.addClass('content-hidden').fadeOut();
+    )};
+});
+
+</script>
+</head>
 <body>
 <div class="wrapper"><h1>Programming</h1></div>
 <div class="welcome-section content-hidden">
@@ -19,4 +44,7 @@
 </ul>
 <a href="{{action('HomeController@index')}}" class="enter-button">Entrar</a>
 </div>
-</div>
+</div>   
+
+</body>
+</html>
