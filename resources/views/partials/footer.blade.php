23e34cdde8e113818a9f2ea7f9bdb5ab6dbc6989
Jonathan <jonstadeepin@gmail.com>
2018-05-17 19:34:21 -0300

    InteraKids

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,20 +1,3 @@
 <footer>
-    <div class="footer-above">
-        <div class="container">
-            <div class="row">
-                <div class="col-md-4 footer-col">footer-col 1</div>
-                <div class="col-md-4 footer-col">footer-col 2</div>
-                <div class="col-md-4 footer-col">footer-col 3</div>
-            </div>
-        </div>
-    </div>
-    <div class="footer-below">
-        <div class="container">
-            <div class="row">
-                <div class="col-md-12 text-center">
-                    Footer
-                </div>
-            </div>
-        </div>
-    </div>
+   
 </footer>
