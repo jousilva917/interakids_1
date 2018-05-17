09b772dc222256f2fd3ebb85359715a338e563f4
TheWinterhell <raphaelcoelho46@gmail.com>
2018-05-17 18:53:27 -0300

    InteraKids

--- resources/lang/en/auth.php
+++ resources/lang/en/auth.php
@@ -13,7 +13,7 @@ return [
     |
     */
 
-    'failed' => 'These credentials do not match our records.',
+    'failed' => 'Houve um problema com o email ou senha.',
     'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
 
 ];
