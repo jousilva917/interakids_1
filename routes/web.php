6f9785b3ecdc7a9d78da8b7f880c807d22be1e59
TheWinterhell <raphaelcoelho46@gmail.com>
2018-05-17 15:57:55 -0300

    InteraKids

--- routes/web.php
+++ routes/web.php
@@ -12,7 +12,6 @@ Route::get('/', function()
 });
 Route::get('/home', 'HomeController@index');
 Route::get('/aprenda', 'LearnController@aprenda');
-Route::get('/explore', 'HomeController@explore');
 Route::get('/sobre', 'HomeController@sobre');
 Route::get('/contato', 'HomeController@contato');
 Route::get('/task', 'LearnController@task');
