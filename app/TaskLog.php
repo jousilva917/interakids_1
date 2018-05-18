ac4a18eb77a44c52b3b86a6514a73b3aace513f1
TheWinterhell <raphaelcoelho46@gmail.com>
2018-05-17 21:39:50 -0300

    InteraKids

--- app/TaskLog.php
+++ app/TaskLog.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class TaskLog extends Model
 {
-    //
+    private function user_quest_exists()
+    {
+        return self::where('aula_id', $this->aula_id)->where('user_id', $this->user_id)->limit(1)->first();
+    }
 }
