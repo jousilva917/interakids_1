8e6e34181bbf604898b278a9f720a0b555101c42
TheWinterhell <raphaelcoelho46@gmail.com>
2018-05-17 21:13:31 -0300

    InteraKids

--- database/migrations/2018_05_17_220054_add__task_table.php
+++ database/migrations/2018_05_17_220054_add__task_table.php
@@ -15,7 +15,7 @@ class AddTaskTable extends Migration
     {
         Schema::create('task', function(Blueprint $table){
             $table->increments('id');
-            $table->string('nomeAula');
+            $table->string('level');
             $table->timestamps();
         });
     }
