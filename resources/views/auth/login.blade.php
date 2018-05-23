2393f1d1513143ed86069d967a32237216e2bde1
TheWinterhell <raphaelcoelho46@gmail.com>
2018-05-23 09:24:07 -0300

    InteraKids

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -11,7 +11,6 @@
 		            <a href="" class="btn btn-outline-secondary btn-lg"> <i class="fab fa-facebook-f"></i>   Login via facebook</a>
 	            </p>
                 </div>
-                
                 <div class="card-body">
                     <form method="POST" action="{{ route('login') }}">
                         @csrf
@@ -29,13 +28,10 @@
                                 @endif
                             </div>
                         </div>
-
                         <div class="form-group row">
                             <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Senha') }}</label>
-
                             <div class="col-md-6">
                                 <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required>
-
                                 @if ($errors->has('password'))
                                     <span class="invalid-feedback">
                                         <strong>{{ $errors->first('password') }}</strong>
@@ -43,7 +39,6 @@
                                 @endif
                             </div>
                         </div>
-
                         <div class="form-group row">
                             <div class="col-md-6 offset-md-4">
                                 <div class="checkbox">
@@ -53,13 +48,11 @@
                                 </div>
                             </div>
                         </div>
-
                         <div class="form-group row mb-0">
                             <div class="col-md-8 offset-md-4">
                                 <button type="submit" class="btn btn-primary">
                                     {{ __('Login') }}
                                 </button>
-
                                 <a class="btn btn-link" href="{{ route('password.request') }}">
                                     {{ __('Esqueceu a senha?') }}
                                 </a>
