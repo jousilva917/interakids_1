f76247aedc16f74c2913d005adba968fb4c2a749
TheWinterhell <raphaelcoelho46@gmail.com>
2018-05-24 10:59:54 -0300

    InteraKids

--- resources/views/partials/nav.blade.php
+++ resources/views/partials/nav.blade.php
@@ -38,11 +38,10 @@
                         <a class="dropdown-item" href="{{ route('logout') }}"
                                 onclick="event.preventDefault();
                                         document.getElementById('logout-form').submit();">
-                                    {{ __('Sair') }}
-                        </a>
-                        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-                             @csrf
-                        </form>
+                                    {{ __('Sair') }}</a>
+                    <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                        @csrf
+                    </form>
                 </div>
             </li>
             @endguest
