d4c83dd79236270119cfc94a05848a7897a74465
07-15 <07-15>
2018-05-25 12:36:41 -0300

    InteraKids

--- resources/views/partials/nav.blade.php
+++ resources/views/partials/nav.blade.php
@@ -1,33 +1,34 @@
 <!-- Navegação -->
+<div class="container">
 <nav class="navbar navbar-expand-lg navbar-light bg-light" id="navbar">
-    <div class="Container">
+
         <a class="navbar-brand" href="{{ url('/home') }}">
             {{ config('app.name', 'InteraKids') }}
         </a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
-            aria-expanded="false" aria-label="Toggle navigation">
+        <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
         <!-- Navbar -->
+        <div id="navbarNavDropdown" class="navbar-collapse collapse" style="justify-content: flex-end;">
         <ul class="navbar-nav">
-            <li class="{{Request::is('home') ? 'active': ''}}">
+            <li class="{{Request::is('home') ? 'active': ''}} nav-item">
                 <a class="nav-link" href="{{ action('HomeController@index') }}">Home</a>
             </li>
-            <li class="{{Request::is('aprenda') ? 'active': ''}}">
+            <li class="{{Request::is('aprenda') ? 'active': ''}} nav-item">
                 <a class="nav-link" href="{{ action('LearnController@aprenda') }}">Aprenda</a>
             </li>
-            <li class="{{Request::is('contato') ? 'active': ''}}">
+            <li class="{{Request::is('contato') ? 'active': ''}} nav-item">
                 <a class="nav-link" href="{{ action('HomeController@contato') }}">Contato</a>
             </li>
-            <li class="{{Request::is('sobre') ? 'active': ''}}">
+            <li class="{{Request::is('sobre') ? 'active': ''}} nav-item">
                 <a class="nav-link" href="{{ action('HomeController@sobre') }}">Sobre</a>
             </li>
             <!-- Authentication Links -->
             @guest
-            <li class="{{Request::is('login') ? 'active': ''}}">
+            <li class="{{Request::is('login') ? 'active': ''}} nav-item">
                 <a class="nav-link " href="{{ route('login') }}">{{ __('Login') }}</a>
             </li>
-            <li class="{{Request::is('register') ? 'active': ''}}">
+            <li class="{{Request::is('register') ? 'active': ''}} nav-item">
                 <a class="nav-link" href="{{ route('register') }}">{{ __('Registrar') }}</a>
             </li>
             @else
@@ -49,6 +50,7 @@
             </li>
             @endguest
         </ul>
-    </div>
+        </div>
     </div>
 </nav>
+</div>
