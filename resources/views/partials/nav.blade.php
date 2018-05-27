764a4de0e5862f13861ca7a6a9eb2b3550345a1d
Jonathan <jonstadeepin@gmail.com>
2018-05-27 11:59:59 -0300

    InteraKids

--- resources/views/partials/nav.blade.php
+++ resources/views/partials/nav.blade.php
@@ -1,35 +1,35 @@
 <!-- Navegação -->
+<body id="page-top">
+<nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
 <div class="container">
-<nav class="navbar navbar-expand-lg navbar-light bg-light" id="navbar">
-
         <a class="navbar-brand" href="{{ url('/home') }}">
             {{ config('app.name', 'InteraKids') }}
         </a>
         <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
+            <i class="fa fa-bars"></i>
         </button>
         <!-- Navbar -->
-        <div id="navbarNavDropdown" class="navbar-collapse collapse  " style="justify-content: flex-end;">
-        <ul class="navbar-nav">
+        <div class="navbar-collapse collapse nav-item dropdown " id="navbarResponsive">
+        <ul class="navbar-nav text-uppercase ml-auto">
             <li class="{{Request::is('home') ? 'active': ''}} nav-item">
-                <a id="btn-1" class="nav-link" href="{{ action('HomeController@index') }}"><i class="fa fa-home fa-2x" id="icons-1"></i> inicial </a>
+                <a id="btn-1" class="nav-link" href="{{ action('HomeController@index') }}"> inicial </a>
             </li>
             <li class="{{Request::is('aprenda') ? 'active': ''}} nav-item">
-                <a id="btn-2" class="nav-link" href="{{ action('LearnController@aprenda') }}"><i class="" id="icons-2"></i>Explore</a>
+                <a id="btn-2" class="nav-link" href="{{ action('LearnController@aprenda') }}">Explore</a>
             </li>
             <li class="{{Request::is('contato') ? 'active': ''}} nav-item">
-                <a id="btn-3" class="nav-link" href="{{ action('HomeController@contato') }}"><i class="" id="icons-3"></i>Contato</a>
+                <a id="btn-3" class="nav-link" href="{{ action('HomeController@contato') }}">Contato</a>
             </li>
             <li class="{{Request::is('sobre') ? 'active': ''}} nav-item">
-                <a id="btn-4" class="nav-link" href="{{ action('HomeController@sobre') }}"><i class="" id="icons-4"></i>Sobre</a>
+                <a id="btn-4" class="nav-link" href="{{ action('HomeController@sobre') }}">Sobre</a>
             </li>
             <!-- Authentication Links -->
             @guest
             <li class="{{Request::is('login') ? 'active': ''}} nav-item">
-                <a id="btn-5" class="nav-link" href="{{ route('login') }}"><i class="" id="icons-5"></i>{{ __('Login') }}</a>
+                <a id="btn-5" class="nav-link" href="{{ route('login') }}">{{ __('Login') }}</a>
             </li>
             <li class="{{Request::is('register') ? 'active': ''}} nav-item">
-                <a id="btn-6" class="nav-link" href="{{ route('register') }}"><i class="" id="icons-6"></i>{{ __('Registrar') }}</a>
+                <a id="btn-6" class="nav-link" href="{{ route('register') }}">{{ __('Registrar') }}</a>
             </li>
             @else
             <li class="nav-item dropdown">
@@ -53,3 +53,4 @@
         </div>
     </div>
 </nav>
+</body>
--- resources/views/website/home.blade.php
+++ resources/views/website/home.blade.php
@@ -2,46 +2,54 @@
 <link href="{{ asset('css/parallax.css') }}" rel="stylesheet">
 <div id="loader" class="loader"></div>
 <div style="display:none;" id="tudo_page">
-  <header class="header">
-    <div class="landing">
-      <div id='hero'>
-        <div class='layer-bg layer' data-depth='0.10' data-type='parallax'></div>
-        <div class='layer-1 layer' data-depth='0.20' data-type='parallax'></div>
-        <div class='layer-2 layer' data-depth='0.50' data-type='parallax'></div>
-        <div class='layer-3 layer' data-depth='0.80' data-type='parallax'></div>
-        <div class='layer-overlay layer' data-depth='0.85' data-type='parallax'></div>
-        <div class='layer-4 layer' data-depth='1.00' data-type='parallax'></div>
+
+  <!-- Header -->
+  <header class="masthead">
+    <div class="container">
+      <div class="intro-text">
+        <a class="btn btn-primary btn-xl text-uppercase js-scroll-trigger" href="#">Aprenda</a>
       </div>
     </div>
   </header>
-  <div id='content'>
-    <div class='container'>
-      <section class='first-section'>
-        <div class='row'>
-          <div class='col-sm-6'>
-            <h1>You cannot hide the soul. Through all his unearthly tattooings, I thought I saw the traces of a simple honest
-              heart.
-            </h1>
-          </div>
-        </div>
-        <div class='row'>
-          <div class='col-sm-6'>
-            <p>And besides all this, there was a certain lofty bearing about the Pagan, which even his uncouthness could not
-              altogether maim. He looked like a man who had never cringed and never had had a creditor. Whether it was, too,
-              that his head being shaved, his forehead was drawn out in freer and brighter relief, and looked more expansive
-              than it otherwise would, this I will not venture to decide; but certain it was his head was phrenologically
-              an excellent one.</p>
-          </div>
-          <div class="col-sm-6">
-            <iframe width="420" height="315" src="https://www.youtube.com/embed/tgbNymZ7vqY">
-            </iframe>
-          </div>
-        </div>
-      </section>
-    
 
-
+  <section id="services">
+    <div class="container">
+      <div class="row">
+        <div class="col-lg-6 text-center">
+          <h2 class="section-heading text-uppercase">InteraKids</h2>
+          <h3 class="section-subheading text-muted">Lorem ipsum dolor sit amet consectetur.</h3>
+          <a class="btn btn-primary btn-xl" href="#" style="position:flex; right:200px;">Aprenda</a>
+        </div>
+        <div class="col-lg-6">
+          <iframe width="500" height="400" src="https://www.youtube.com/embed/tgbNymZ7vqY">
+          </iframe>
+        </div>
+      </div>
+    </div>
+</div>
+</section>
+<section class="grid">
+  <div class="container">
+  
+    <div class="row">
+     <div class="col-lg-12">
+     <h1>Grid</h1>     
+    </div>
+      <div class="col-sm-4 text-center"><h1>ola</h1>
+      </div>
+      <div class="col-sm-4 text-center"><h1>ola</h1>
+      </div>
+      <div class="col-sm-4 text-center"><h1>ola</h1>
+      </div>
+      <div class="col-sm-4 text-center"><h1>ola</h1>
+      </div>
+      <div class="col-sm-4 text-center"><h1>ola</h1>
+      </div>
+      <div class="col-sm-4 text-center"><h1>ola</h1>
+      </div>
     </div>
   </div>
-</div>
-@endsection
+
+  </div>
+  </div>
+  @endsection
