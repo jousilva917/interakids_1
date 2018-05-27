b87cd9c91daf91964a388f39a6ea8e413f9b9594
Jonathan <Jousilva917@gmail.com>
2018-05-26 23:13:49 -0300

    InteraKids

--- resources/views/website/home.blade.php
+++ resources/views/website/home.blade.php
@@ -20,7 +20,8 @@
         <div class='row'>
           <div class='col-sm-6'>
             <h1>You cannot hide the soul. Through all his unearthly tattooings, I thought I saw the traces of a simple honest
-              heart.</h1>
+              heart.
+            </h1>
           </div>
         </div>
         <div class='row'>
@@ -30,32 +31,16 @@
               that his head being shaved, his forehead was drawn out in freer and brighter relief, and looked more expansive
               than it otherwise would, this I will not venture to decide; but certain it was his head was phrenologically
               an excellent one.</p>
-            <p>It may seem ridiculous, but it reminded me of General Washington's head, as seen in the popular busts of him.
-              It had the same long regularly graded retreating slope from above the brows, which were likewise very projecting,
-              like two long promontories thickly wooded on top. Queequeg was George Washington cannibalistically developed.</p>
-            <p>Whilst I was thus closely scanning him, half-pretending meanwhile to be looking out at the storm from the casement,
-              he never heeded my presence, never troubled himself with so much as a single glance; but appeared wholly occupied
-              with counting the pages of the marvellous book. Considering how sociably we had been sleeping together the
-              night previous, and especially considering the affectionate arm I had found thrown over me upon waking in the
-              morning, I thought this indifference of his very strange. But savages are strange beings; at times you do not
-              know exactly how to take them.</p>
           </div>
-          <div class='col-sm-6'>
-            <p>At first they are overawing; their calm self-collectedness of simplicity seems a Socratic wisdom. I had noticed
-              also that Queequeg never consorted at all, or but very little, with the other seamen in the inn. He made no
-              advances whatever; appeared to have no desire to enlarge the circle of his acquaintances. All this struck me
-              as mighty singular; yet, upon second thoughts, there was something almost sublime in it. Here was a man some
-              twenty thousand miles from home, by the way of Cape Horn, that is—which was the only way he could get there—thrown
-              among people as strange to him as though he were in the planet Jupiter; and yet he seemed entirely at his ease;
-              preserving the utmost serenity; content with his own companionship; always equal to himself.</p>
-            <p>Here was a man some twenty thousand miles from home, by the way of Cape Horn, that is—which was the only way
-              he could get there—thrown among people as strange to him as though he were in the planet Jupiter; and yet he
-              seemed entirely at his ease; preserving the utmost serenity; content with his own companionship; always equal
-              to himself. Surely this was a touch of fine philosophy; though no doubt he had never heard there was such a
-              thing as that.</p>
+          <div class="col-sm-6">
+            <iframe width="420" height="315" src="https://www.youtube.com/embed/tgbNymZ7vqY">
+            </iframe>
           </div>
         </div>
       </section>
+    
+
+
     </div>
   </div>
 </div>
