4432a98374f6643628c252f66af90f10f81be70e
SILVIO RAPHAEL COELHO <silvio.coelho@etec.sp.gov.br>
2018-05-29 19:01:55 -0300

    InteraKids

--- resources/views/website/contact.blade.php
+++ resources/views/website/contact.blade.php
@@ -1,11 +1,30 @@
 @extends('layouts.website')
 @section('content')
-<div class="container-fluid" id="contact1">         
-    <form action="" method="post">             
-        <label for="">Nome</label>                 
-        <input type="text">                 
-        <label for="">E-Mail</label>                 
-        <input type="text">         
-    </form>     
-</div>
+<div class="container-fluid mb-5" id="contact1">         
+    <div class="col-md-5">
+        <div class="form-area">  
+            <form role="form">
+            <br style="clear:both">
+                <h3 style="margin-bottom: 25px; text-align: center;">Contact Form</h3>
+        <div class="form-group">
+            <input type="text" class="form-control" id="name" name="name" placeholder="Name" required>
+        </div>
+        <div class="form-group">
+            <input type="text" class="form-control" id="email" name="email" placeholder="Email" required>
+        </div>
+        <div class="form-group">
+            <input type="text" class="form-control" id="mobile" name="mobile" placeholder="Mobile Number" required>
+        </div>
+        <div class="form-group">
+            <input type="text" class="form-control" id="subject" name="subject" placeholder="Subject" required>
+        </div>
+        <div class="form-group">
+            <textarea class="form-control" type="textarea" id="message" placeholder="Message" maxlength="140" rows="7"></textarea>
+            <span class="help-block"><p id="characterLeft" class="help-block ">You have reached the limit</p></span>                    
+        </div>   
+        <button type="button" id="submit" name="submit" class="btn btn-primary pull-right">Submit Form</button>
+            </form> 
+        </div>
+    </div>
+</div>     
 @endsection
