54a26f26bdbf15938fbc19758265d24d234a9343
JONATHAN MICAEL OLIVEIRA SILVA <jonathan.silva294@etec.sp.gov.br>
2018-06-03 12:26:48 -0300

    interakids

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,1 +1,29 @@
 
+ <footer id="myFooter">
+                <div class="container">
+                    <div class="row">
+                        <div class="col-sm-3">
+                            <h2 class="logo"><a href="#"> LOGO </a></h2>
+                        </div>
+                        <div class="col-sm-2">
+                            <h5>Get started</h5>
+                            <ul>
+                                <li><a href="#">Home</a></li>
+                                <li><a href="#">Sign up</a></li>
+                                <li><a href="#">About</a></li>
+                            </ul>
+                        </div>
+                        <div class="col-sm-3">
+                            <div class="social-networks">
+                                <a href="#" class="twitter"><i class="fa fa-twitter"></i></a>
+                                <a href="#" class="facebook"><i class="fa fa-facebook"></i></a>
+                                <a href="#" class="google"><i class="fa fa-google-plus"></i></a>
+                            </div>
+                            <button type="button" class="btn btn-default">Contact us</button>
+                        </div>
+                    </div>
+                </div>
+                <div class="footer-copyright">
+                    <p>© 2018 Feito com amor e cofee </p>
+                </div>
+            </footer>
