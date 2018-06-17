c5af198cb2c2558cb540c6fdc540af66a8edc834
SILVIO RAPHAEL COELHO <silvio.coelho@etec.sp.gov.br>
2018-06-17 18:34:50 -0300

    InteraKids

--- resources/views/learn/perfil.blade.php
+++ resources/views/learn/perfil.blade.php
@@ -6,7 +6,7 @@
 <meta http-equiv="X-UA-Compatible" content="IE=edge">
 <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 {!! Minify::stylesheet(['/css/site/app.css', '/css/learn/sb-admin.css', '/css/learn/font-awesome.min.css'])->withFullUrl() !!}
-{!! Minify::javascript(['/js/site/app.js'])!!}
+{!! Minify::javascript(['/js/site/app.js'])->withFullUrl()!!}
 @include('learn.picture')
 @endsection
 
