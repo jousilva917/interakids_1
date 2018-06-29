ceeeb1340e476909cede84f282aa9560fca2e545
SILVIO RAPHAEL COELHO <silvio.coelho@etec.sp.gov.br>
2018-06-29 11:11:33 -0300

    InteraKids

--- resources/views/website/home.blade.php
+++ resources/views/website/home.blade.php
@@ -8,51 +8,6 @@
 <div class="" style="display:none;" id="page-secundaria">
     <!--HEADER-->
 
-<<<<<<< HEAD
-  <div id='hero'>
-    <div class='layer-bg layer' data-depth='0.70' data-type='parallax'></div>
-    <div class='layer-1 layer' data-depth='0.25' data-type='parallax'></div>
-    <div class='layer-2 layer' data-depth='0.63' data-type='parallax'></div>
-    <div class='layer-3 layer' data-depth='0.90' data-type='parallax'></div>
-    <div class='layer-overlay layer' data-depth='0.85' data-type='parallax'></div>
-    <div class='layer-4 layer' data-depth='1.00' data-type='parallax'></div>
-</div>
-<div id='content'>
-  <div class='container'>
-    <section class='first-section'>
-      <div class='row'>
-        <div class='col-sm-6'>
-          <h1>InteraKids.</h1>
-        </div>
-      </div>
-      <div class='row'>
-        <div class='col-sm-6'>
-         <p>Whilst I was thus closely scanning him, half-pretending meanwhile to be looking out at the storm from the casement, he never heeded my presence, never troubled himself with so much as a single glance; but appeared wholly occupied with counting the pages of the marvellous book. Considering how sociably we had been sleeping together the night previous, and especially considering the affectionate arm I had found thrown over me upon waking in the morning, I thought this indifference of his very strange.</p>
-        </div>
-        <div class='col-sm-6'>
-          <p>Here was a man some twenty thousand miles from home, by the way of Cape Horn, that is—which was the only way he could get there—thrown among people as strange to him as though he were in the planet Jupiter; and yet he seemed entirely at his ease; preserving the utmost serenity; content with his own companionship; always equal to himself. Surely this was a touch of fine philosophy; though no doubt he had never heard there was such a thing as that.</p>
-          <a class='tutorial-link' href='https://medium.com/@PatrykZabielski/how-to-make-multi-layered-parallax-illustration-with-css-javascript-2b56883c3f27'>
-            Learn how to create this parallax effect
-          </a>
-         <section class="border-btn">
-    <div class="container">
-       <div class="row text-center">
-    
-            <div class="btn-heading"></div>
-            <a class="btn4" href="#">Jogar</a>
-           </div>
-        </div>
-         </section>
-        </div>
-      </div>
-    </section>
-  </div>
-</div>
-
-
-
-
-=======
     <div id='hero'>
         <div class='layer-bg layer' data-depth='0.70' data-type='parallax'></div>
         <div class='layer-1 layer' data-depth='0.25' data-type='parallax'></div>
@@ -87,7 +42,6 @@
                             diversas como matemática e até resolver problemas na vida real. Está preparado para embarcar
                             neste projeto?
                         </p>
->>>>>>> 2ba0cf2c06d825aa50400b60d4c7080c136958c1
 
                     </div>
                 </div>
